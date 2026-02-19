d78912a8b69fb3d099255cf1dee09b96fac417a2
amanialhahry <amanislshahry.00@gmail.com>
2026-02-19 21:40:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,28 @@ moons.forEach((moon, index) => {
 
   moon.style.left = ${x}px;
   moon.style.top = ${y}px;
+
+  function createArc(selector, radius) {
+  const moons = document.querySelectorAll(selector);
+  const arc = moons[0].parentElement;
+
+  const centerX = arc.offsetWidth / 2;
+  const centerY = arc.offsetHeight;
+
+  const startAngle = Math.PI + 0.4;   // بداية القوس
+  const endAngle = 2 * Math.PI - 0.4; // نهاية القوس
+
+  moons.forEach((moon, index) => {
+    const angle = startAngle + (index / (moons.length - 1)) * (endAngle - startAngle);
+
+    const x = centerX + radius * Math.cos(angle) - (moon.offsetWidth / 2);
+    const y = centerY + radius * Math.sin(angle) - (moon.offsetHeight / 2);
+
+    moon.style.left = ${x}px;
+    moon.style.top = ${y}px;
+  });
+}
+
+createArc(".night-moon", 200);
 });
+
