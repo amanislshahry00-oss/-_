0e3e81a659a09fe6a6faf32cdb337c4b261ce670
amanialhahry <amanislshahry.00@gmail.com>
2026-02-19 21:22:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const hour = new Date().getHours();
-const sky = document.querySelector(".sky");
+const moons = document.querySelectorAll(".moon.small");
+const orbit = document.querySelector(".orbit");
 
-if (hour >= 5 && hour < 17) {
-  sky.classList.add("day");
-} else {
-  sky.classList.add("night");
-}
+const radius = 220; // نصف قطر القوس
+const centerX = orbit.offsetWidth / 2;
+const centerY = orbit.offsetHeight;
 
-function openCategory(name) {
-  alert("فتحت قسم: " + name);
-}
+const startAngle = Math.PI;      // بداية من اليسار
+const endAngle = 2 * Math.PI;    // نهاية عند اليمين
+
+moons.forEach((moon, index) => {
+  const angle = startAngle + (index / (moons.length - 1)) * (endAngle - startAngle);
+
+  const x = centerX + radius * Math.cos(angle) - 35;
+  const y = centerY + radius * Math.sin(angle) - 35;
+
+  moon.style.left = ${x}px;
+  moon.style.top = ${y}px;
+});
