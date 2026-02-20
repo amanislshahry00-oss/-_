c1f971223dd470253bd40ad564391c25031ba943
amanialhahry <amanislshahry.00@gmail.com>
2026-02-21 02:06:07 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const hour = new Date().getHours();
+const body = document.body;
+const modeTitle = document.getElementById("modeTitle");
+
+if (hour >= 5 && hour < 17) {
+    body.classList.add("day");
+    modeTitle.innerText = "رحلة أذكار الصباح";
+} else {
+    body.classList.add("night");
+    modeTitle.innerText = "رحلة أذكار المساء";
+}
+
+const moons = document.querySelectorAll(".moon");
+
+moons.forEach((moon, index) => {
+
+    if (localStorage.getItem("done_" + index)) {
+        moon.classList.add("done");
+    }
+
+    moon.onclick = () => {
+        localStorage.setItem("currentMoon", index);
+        window.location.href = "azkar.html";
+    };
+});
