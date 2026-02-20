2029747bf54cc9bdba18744271c1773d1d9c8fda
amanialhahry <amanislshahry.00@gmail.com>
2026-02-21 02:18:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// الوضع صباح/ليل تلقائي
 const hour = new Date().getHours();
 const body = document.body;
 const modeTitle = document.getElementById("modeTitle");
@@ -10,14 +11,13 @@ if (hour >= 5 && hour < 17) {
     modeTitle.innerText = "رحلة أذكار المساء";
 }
 
+// الأقمار
 const moons = document.querySelectorAll(".moon");
 
 moons.forEach((moon, index) => {
-
     if (localStorage.getItem("done_" + index)) {
         moon.classList.add("done");
     }
-
     moon.onclick = () => {
         localStorage.setItem("currentMoon", index);
         window.location.href = "azkar.html";
