69340f2a8abc54094d4e1cb73c76311ab2801563
amanialhahry <amanislshahry.00@gmail.com>
2026-02-21 03:11:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const buttons = document.querySelectorAll('.sectionBtn');
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
         const section = btn.getAttribute('data-section');
-        alert('تم اختيار قسم: ' + section);
-        // هنا يمكن إضافة فتح صفحة القسم أو تحميل الأذكار
+       
+        const adhkar = {
+  morning: "أذكار الصباح: سبحان الله وبحمده، سبحان الله العظيم...",
+  evening: "أذكار المساء: أعوذ بكلمات الله التامات من شر ما خلق...",
+  sleep: "أذكار النوم: باسمك اللهم أحيا وأموت...",
+  travel: "أذكار السفر: سبحان الذي سخر لنا هذا وما كنا له مقرنين..."
+};
     });
 });
+
